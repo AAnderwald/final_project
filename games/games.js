0521612d43ec73c20bcee7f3314470be1879b0bd
Anna Anderwald <anna.v.anderwald@gmail.com>
2021-07-30 16:07:53 -0400

    more added, a few pages and changes to styel

--- games/games.js
+++ games/games.js
@@ -1,10 +1,26 @@
-const toggle = document.getElementById("toggle");
+// const toggle = document.getElementById("toggle");
 
-toggle.addEventListener("click", ()=>{
+// toggle.addEventListener("click", ()=>{
 
-    if(toggle.innerText === "I do nothing"){
-        toggle.innerText = "Keep pressing nothing will happen";
-    }else{
-        toggle.innerText= "Since you like to click go play Ben's game";
-    }
-});
+//     if(toggle.innerText === "I do nothing"){
+//         toggle.innerText = "Keep pressing nothing will happen";
+//     }else{
+//         toggle.innerText= "Since you like to click go play Ben's game";
+//     }
+// });
+
+// or
+'use strict';
+let count = 0;
+
+let button = document.getElementById('toggle');
+let messages = [ "I am a button", "I do nothing", "Keep pressing nothing will happen", "stop, just stop", "Since you like to click go play Ben's game"];
+function go(e){  
+    button.innerHTML = messages[count];
+    if (count == messages.length -1){  
+        count = 0 
+    } 
+    else{
+        count++;
+    } 
+} button.addEventListener('click', go);
